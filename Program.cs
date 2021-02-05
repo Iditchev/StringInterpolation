c4315f2a7b64607782ea8384823eae3b5c6384c4
iditchev <iditchev@my.wctc.edu>
2021-02-04 18:37:35 -0600

    Reading From File

--- Program.cs
+++ Program.cs
@@ -55,7 +55,33 @@ namespace SleepData
             }
             else if (resp == "2")
             {
-                // TODO: parse data file
+                
+                         // read data from file
+                         StreamReader sr = new StreamReader("data.txt");
+                         while (!sr.EndOfStream)
+                         {
+                             string line = sr.ReadLine();
+                             string[] arr = line.Split(',');
+                             string hours = arr[1];
+                             string[] sleephours = hours.Split('|');
+
+                             // display array data
+                             Console.WriteLine("Course: {0}, Grade: {1}", arr[0], arr[1]);
+                             // add to accumulators
+                             gradePoints += arr[1] == "A" ? 4 : arr[1] == "B" ? 3 : arr[1] == "C" ? 2 : arr[1] == "D" ? 1 : 0;
+                             count += 1;
+                         }
+                         sr.Close();
+                         // calculate GPA
+                         double GPA = (double)gradePoints / count;
+                         Console.WriteLine("GPA: {0:n2}", GPA);
+                     }
+                     
+                     
+                         
+                     
+                 }
+
 
             }
         }
